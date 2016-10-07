fcee12316e43d3115813bd4c82b55ed7b6ce53d2
abigailmuir <abby@motivatedesign.com>
2016-10-07 13:54:01 -0400

    Added files

---
+++ js/project.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".hbo").onClick.addClass("focus");
+  if (".hbo").hasClass("focus") {
+  	$(".channelbutton").addClass("channelfocus"),
+  }
+  $(".showtime").onClick.addClass(".focus");
+  
+
+});
