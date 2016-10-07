d3ec5eee50d7df2071a1e8c316b276bc52fe9f1e
motivate-design <abby@motivatedesign.com>
2016-10-07 14:33:03 -0400

    Updating gh-pages from master branch via GitBot

--- js/project.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(".hbo").onClick.addClass("focus");
-  if (".hbo").hasClass("focus") {
-  	$(".channelbutton").addClass("channelfocus"),
-  }
-  $(".showtime").onClick.addClass(".focus");
-  
-
-});
